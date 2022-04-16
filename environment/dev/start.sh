4ba970b2ccaf889cacdfb429f12d00c45633e78a
Rodolfo De Nadai <rdenadai@gmail.com>
2022-04-16 17:36:43 -0300

    Docker and debug inside of it

---
+++ environment/dev/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker-compose -f stack.dev.yml down
+
+docker-compose -f stack.dev.yml up &
+
+sleep 5
+
+aws --endpoint-url=http://localhost:4566 dynamodb create-table --table-name sensorial.astronomy --attribute-definitions AttributeName=id,AttributeType=S --key-schema AttributeName=id,KeyType=HASH --provisioned-throughput ReadCapacityUnits=2,WriteCapacityUnits=2
+aws --endpoint-url=http://localhost:4566 dynamodb create-table --table-name sensorial.earthquake --attribute-definitions AttributeName=id,AttributeType=S --key-schema AttributeName=id,KeyType=HASH --provisioned-throughput ReadCapacityUnits=2,WriteCapacityUnits=2
+aws --endpoint-url=http://localhost:4566 dynamodb create-table --table-name sensorial.forecast --attribute-definitions AttributeName=id,AttributeType=S --key-schema AttributeName=id,KeyType=HASH --provisioned-throughput ReadCapacityUnits=2,WriteCapacityUnits=2
