d26db77ab4798ddf1d18d22f087625311ce53ce4
Rodolfo De Nadai <rdenadai@gmail.com>
2022-11-26 11:08:38 -0300

    fix: Changed haversine formula;

--- app/business/geo.py
+++ app/business/geo.py
@@ -91,12 +91,12 @@ def haversine(lng1: float, lat1: float, lng2: float, lat2: float) -> int:
     Calculate the great circle distance between two points
     on the earth (specified in decimal degrees)
     """
-    # convert decimal degrees to radians
-    lon1, lat1, lon2, lat2 = map(radians, [lng1, lat1, lng2, lat2])
-
+    dlat = radians(lat2 - lat1)
+    dlon = radians(lng2 - lng1)
+    # convert to radians
+    lat1 = radians(lat1)
+    lat2 = radians(lat2)
     # haversine formula
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
     a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
     c = 2 * asin(sqrt(a))
     r = 6371  # Radius of earth in kilometers. Use 3956 for miles
